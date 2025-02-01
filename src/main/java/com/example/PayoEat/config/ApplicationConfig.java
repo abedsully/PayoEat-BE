cf94fd217bc3864e764740213e6f1f51600e275e
abedsully <abedsully2909@gmail.com>
2025-02-01 13:16:58 +0700

    feat: added model mapper and dto

---
+++ src/main/java/com/example/PayoEat/config/ApplicationConfig.java
@@ -0,0 +1,13 @@
+package com.example.PayoEat.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.modelmapper.ModelMapper;
+
+@Configuration
+public class ApplicationConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
